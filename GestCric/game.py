04cd657410f595f8a73c81b260499d7277ebc0ed
Shrashtika Singh <66116130+Shrashtika@users.noreply.github.com>
2021-05-09 21:01:38 +0530

    Update game.py

--- GestCric/game.py
+++ GestCric/game.py
@@ -21,24 +21,28 @@ def printscore(s1,s2):
     cv2.waitKey(0)
 
 def printresult(s1, s2):
+    img = np.zeros((512, 512, 3), np.uint8)
+    cv2.rectangle(img, (150, 156), (362, 356), (0, 255, 0), cv2.FILLED)
+    cv2.putText(img, "Player Score", (110, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+    cv2.putText(img, str(s1), (156, 306), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 8)
+
     if s1>s2:
-        img = np.zeros((256, 256, 3), np.uint8)
-        cv2.putText(img, "PLAYER", (45, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-        cv2.putText(img, "WIN", (75, 200), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        cv2.putText(img, "PLAYER WIN", (125, 450), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
+        #cv2.putText(img, "WIN", (75, 200), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
         cv2.imshow('Image', img)
         cv2.waitKey(0)
 
     elif s1<s2:
-        img = np.zeros((256, 256, 3), np.uint8)
-        cv2.putText(img, "COMPUTER", (5, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-        cv2.putText(img, "WIN", (75, 200), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        cv2.putText(img, "COMPUTER WIN", (85, 450), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 255), 3)
+        #cv2.putText(img, "WIN", (75, 200), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
         cv2.imshow('Image', img)
         cv2.waitKey(0)
 
     else:
-        img = np.zeros((256, 256, 3), np.uint8)
-        cv2.putText(img, "TIE", (55, 150), cv2.FONT_HERSHEY_PLAIN, 7, (255, 0, 0), 5)
+
+        cv2.putText(img, "TIE", (165, 470), cv2.FONT_HERSHEY_PLAIN, 7, (0, 0, 255), 5)
         cv2.imshow('Image', img)
         cv2.waitKey(0)
-
 
